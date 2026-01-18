c09a061011171935108906620455e1389a968995
Pranavjeet <pranavjeetmishra@gmail.com>
2026-01-18 23:25:32 +0530

    app file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
 import React from 'react'
 
 export default function App() {
+cons
+
   return (
     <div className="app">
       <header>
         <h1>Product Listing</h1>
       </header>
-      <main>
-        <p>Welcome — scaffold using React 19 + Vite.</p>
-      </main>
+     {location.page ==='list' && (
+        <div>
+            Product Listing
+        </div>
+     )
+     }
+
+
+     {
+        location.page ==='view' && (
+        <div>
+            Product View
+        </div>
+     )
+     }
+
+     {
+        localation.page !== 'list' && (
+            <div>
+                <button onClick={() => navigateTo('list')}>Back to Listing</button>
+            </div>
+        )
+     }
     </div>
   )
 }
